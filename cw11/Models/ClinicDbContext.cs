f7a208d47e7954efc38ff84bebc1973bdd740c4d
Tomasz Jóźwik <s18929@pjwstk.edu.pl>
2020-05-18 15:18:17 +0200

    Początek

---
+++ cw11/Models/ClinicDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cw11.Models
+{
+    public class ClinicDbContext : DbContext
+    {
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Prescription> Prescriptions { get; set; }
+        public DbSet<Medicament> Medicaments { get; set; }
+        public DbSet<PrescriptionMedicament> Prescription_Medicament { get; set; }
+
+        public ClinicDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+    }
+}
---
+++ cw11/Services/IClinicDbService.cs
@@ -0,0 +1,16 @@
+﻿using cw11.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cw11.Services
+{
+    public interface IClinicDbService
+    {
+        public Doctor GetDoctors();
+        public Doctor AddDoctor();
+        public Doctor ModifyDoctor();
+        public Doctor DeleteDoctor(int IdDoctor);
+    }
+}
